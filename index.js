47a7e1189bdcc618feeb11bbcdbb3546df5d4a86
lucas rego <lucasalvesrego.contato@gmail.com>
2022-10-03 09:05:29 -0300

    feat: publish

--- index.js
+++ index.js
@@ -18,25 +18,25 @@ export default function rehypeBase64ImageExtractor({ handler }) {
 
       const findBase64 = /data:([^"]+)*/gm;
 
-      const isBase64 = src.match(findBase64);
+      const isBase64 = src?.match(findBase64);
 
       if (isBase64 === null) return;
 
       const findBase64Mimetype = /data:(.*);base64/gi;
 
-      const mimetypeWithWrapper = src.match(findBase64Mimetype)[0];
+      // Ex: data:image/png;base64
+      const mimetypeWithWrapper = src?.match(findBase64Mimetype)[0];
 
+      // Ex: image/png
       const mimetype = mimetypeWithWrapper
-        .replace("data:", "")
-        .replace(";base64", "");
+        ?.replace("data:", "")
+        ?.replace(";base64", "");
 
-      const data = {
+      // The handler method should return an url for the image
+      const url = await handler({
         mimetype: mimetype,
         content: src,
-      };
-
-      // Handler should return an url
-      const url = await handler(data);
+      });
 
       // Changes the original img src
       const replacement = {
